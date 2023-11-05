08f0e7af3865915cb8276a148f6e670e5e9ca313
Xicopz <121362589+xicopZ@users.noreply.github.com>
2023-11-05 22:33:36 +0000

    Add files via upload

---
+++ PyMask.py
@@ -0,0 +1,17 @@
+ascii_code_1 = '''
+
+██████╗ ██╗   ██╗███╗   ███╗ █████╗ ███████╗██╗  ██╗
+██╔══██╗╚██╗ ██╔╝████╗ ████║██╔══██╗██╔════╝██║ ██╔╝
+██████╔╝ ╚████╔╝ ██╔████╔██║███████║███████╗█████╔╝ 
+██╔═══╝   ╚██╔╝  ██║╚██╔╝██║██╔══██║╚════██║██╔═██╗ 
+██║        ██║   ██║ ╚═╝ ██║██║  ██║███████║██║  ██╗
+╚═╝        ╚═╝   ╚═╝     ╚═╝╚═╝  ╚═╝╚══════╝╚═╝  ╚═╝
+                                                    
+'''
+
+print(ascii_code_1)
+print('by Xicopz')
+original_link = input('Paste the link you would like to mask (with http or https): ')
+fake_domain = input('''Enter the domain you would like you site to appear as (with http or https)
+ eg: https://instagram.com: ''')
+social_enginnering = input('''Enter social enginnering words. eg: free-robux''')
